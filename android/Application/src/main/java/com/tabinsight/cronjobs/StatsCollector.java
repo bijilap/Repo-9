5282aa1d66bb891a255fa3988fdf5e844313bb9c
Bijil Philip <biphilip@paypal.com>
2015-10-03 22:29:50 -0700

    integrating db changes with stats collector

--- android/Application/src/main/java/com/tabinsight/cronjobs/StatsCollector.java
+++ android/Application/src/main/java/com/tabinsight/cronjobs/StatsCollector.java
@@ -9,50 +9,59 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.example.android.appusagestatistics.StatsUsageInterval;
+import com.tabinsights.constants.LogTags;
 import com.tabinsights.constants.WhitelisteedApps;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
+import tabinsights.database.AppUseInfo;
+import tabinsights.database.AppsInfoDatasource;
+
 public class StatsCollector extends Service {
     public StatsCollector() {
     }
     UsageStatsManager mUsageStatsManager;
+    AppsInfoDatasource appsInfoDatasource;
+
     @Override
     public IBinder onBind(Intent intent) {
-
-        mUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
-        // TODO: Return the communication channel to the service.
-        HashMap<String, UsageStats> usageStatsMap = getUsageStatsMap();
-        WhitelisteedApps whiteListedApps = new WhitelisteedApps();
-        List<String> packageNames= whiteListedApps.getPackageNames();
-        List<String> log = new ArrayList<>();
-        for(String name: packageNames){
-            UsageStats usageStat = usageStatsMap.get(name);
-            Log.d("TEST123",usageStat.getLastTimeUsed()+"\tdummydevice"+usageStat.getPackageName()+"\t"+usageStat.getTotalTimeInForeground());
-        }
-        //throw new UnsupportedOperationException("Not yet implemented");
         return null;
     }
 
 
     @Override
     public void onCreate() {
+
+        appsInfoDatasource = new AppsInfoDatasource(getBaseContext());
         mUsageStatsManager = (UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
         // TODO: Return the communication channel to the service.
         HashMap<String, UsageStats> usageStatsMap = getUsageStatsMap();
         WhitelisteedApps whiteListedApps = new WhitelisteedApps();
         List<String> packageNames= whiteListedApps.getPackageNames();
         List<String> log = new ArrayList<>();
-        for(String name: packageNames){
-            if(!usageStatsMap.containsKey(name)){
-                continue;
+        try {
+            appsInfoDatasource.open();
+            for (String name : packageNames) {
+                if (!usageStatsMap.containsKey(name)) {
+                    continue;
+                }
+                UsageStats usageStat = usageStatsMap.get(name);
+                Log.d(LogTags.APP_INFO.name(), usageStat.getLastTimeUsed() + "\tdummydevice" + usageStat.getPackageName() + "\t" + usageStat.getTotalTimeInForeground());
+                appsInfoDatasource.createEntry(name, usageStat.getLastTimeStamp() + "", usageStat.getTotalTimeInForeground());
             }
-            UsageStats usageStat = usageStatsMap.get(name);
-            Log.d("TEST123",usageStat.getLastTimeUsed()+"\tdummydevice"+usageStat.getPackageName()+"\t"+usageStat.getTotalTimeInForeground());
         }
+        catch(SQLException e){
+            Log.d(LogTags.APP_EXCEPTION.name(), "SQLException");
+        }
+    }
+
+    @Override
+    public void onDestroy(){
+        appsInfoDatasource.close();
     }
 
     private HashMap<String, UsageStats> getUsageStatsMap(){
---
+++ android/Application/src/main/java/com/tabinsights/constants/LogTags.java
@@ -0,0 +1,9 @@
+package com.tabinsights.constants;
+
+/**
+ * Created by biphilip on 10/3/15.
+ */
+public enum LogTags {
+    APP_EXCEPTION,
+    APP_INFO
+}
